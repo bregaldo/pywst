c5f7d0ba883079356417674d909ee9cacd7c4561
Bruno Régaldo-Saint Blancard <bruno.regaldo@gmail.com>
2020-05-07 18:21:05 +0200

    setup.py added.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pywst",
+    version="0.1",
+    author="Bruno Régaldo-Saint Blancard",
+    author_email="bruno.regaldo@phys.ens.fr",
+    description="WST and RWST analyses tools for astrophysical data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bregaldo/pywst",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: ???? License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
